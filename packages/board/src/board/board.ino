06d27c6fa072c3a05995932e06d697b5284efd1e
mrohmer <rohmer.matze@gmail.com>
2024-06-26 23:01:33 +0200

    feat: make flags blink

--- packages/board/src/board/board.ino
+++ packages/board/src/board/board.ino
@@ -8,22 +8,36 @@
 #define FLAGS_NUM 5
 #define FLAGS_NUMPIXELS FLAGS_PIXELS_PER_FLAG * FLAGS_NUM
 
-#define MS_CYCLE 100
+#define FLAGS_STATE_RED 0
+#define FLAGS_STATE_GREEN 1
+#define FLAGS_STATE_CHAOS 2
+
+#define MS_CYCLE 250
 #define MS_BETWEEN_LED_TOGGLE 1000
+#define MS_BETWEEN_FLAGS_GREEN 500
+#define MS_BETWEEN_FLAGS_CHAOS 250
+#define MS_BETWEEN_FLAGS_RED 1000
 
 #include <Arduino.h>
 #include <string.h>
 #include <math.h>
 #include <Adafruit_NeoPixel.h>
 
+struct LastExecutionState {
+    unsigned long ledToggle;
+    unsigned long flagsStart;
+    unsigned long flagsChaos;
+};
+struct FlagsState {
+    int state;
+    int toggles;
+};
+
 Adafruit_NeoPixel pitlane = Adafruit_NeoPixel(PITLANE_NUMPIXELS, PIN_FLAGS, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel falgs = Adafruit_NeoPixel(FLAGS_NUMPIXELS, PIN_FLAGS, NEO_GRB + NEO_KHZ800);
 
-struct LastExecutionState {
-    unsigned long ledToggle;
-};
-
 struct LastExecutionState lastExecution;
+struct FlagsState flagsState;
 
 // --- Declarations ---
 #if ESP
@@ -47,9 +61,21 @@ void setupFlags();
 
 void cycleToggleStatusLed();
 
+void cycleUpdateFlagsGreen();
+
+void cycleUpdateFlagsChaos();
+
+void cycleUpdateFlagsRed();
+
 void toggleStatusLed();
 
 void flashStatusLed(int n);
+
+void updateFlagsGreen();
+
+void updateFlagsChaos();
+
+void updateFlagsRed();
 
 // --- Arduino Loop ---
 void setup() {
@@ -66,19 +92,36 @@ void setup() {
 
 void loop() {
   cycleToggleStatusLed();
+  cycleUpdateFlagsGreen();
+  cycleUpdateFlagsChaos();
+  cycleUpdateFlagsRed();
 
   delay(MS_CYCLE);
 }
 
 // --- Cycle ---
 bool shouldExecuteInThisCycle(int difference, unsigned long lastExecution) {
-  return ((millis() - lastExecution) % difference) < MS_CYCLE;
+  return ((millis() - lastExecution) % difference) <= MS_CYCLE;
 }
 
 void cycleToggleStatusLed() {
   if (shouldExecuteInThisCycle(MS_BETWEEN_LED_TOGGLE, lastExecution.ledToggle)) {
     toggleStatusLed();
     lastExecution.ledToggle = millis();
+  }
+}
+
+void cycleUpdateFlagsGreen() {
+  if (shouldExecuteInThisCycle(MS_BETWEEN_FLAGS_GREEN, lastExecution.flagsGreen)) {
+    updateFlagsGreen();
+    lastExecution.flagsGreen = millis();
+  }
+}
+
+void cycleUpdateFlagsChaos() {
+  if (shouldExecuteInThisCycle(MS_BETWEEN_FLAGS_CHAOS, lastExecution.flagsChaos)) {
+    updateFlagsChaos();
+    lastExecution.flagsChaos = millis();
   }
 }
 
@@ -135,28 +178,83 @@ void updatePitlanes(bool lane1, bool lane2) {
 
 // --- FLAGS ---
 void setupFlags() {
+  flagsState = {
+          state = FLAGS_STATE_RED,
+          toggles = 0
+  }
   flags.begin();
   flags.setBrightness(15);
   flags.clear();
   flags.show();
 }
 
-void setFlagsYellow() {
-  flags.clear();
-  flags.fill(flags.Color(255, 255, 255), 0, FLAGS_NUMPIXELS);
-  flags.show();
+void setFlagsChaos() {
+  flagsState = {
+          state = FLAGS_STATE_CHAOS,
+          toggles = 0
+  }
 }
 
 void setFlagsRed() {
-  flags.clear();
-  flags.fill(flags.Color(255, 0, 0), 0, FLAGS_NUMPIXELS);
-  flags.show();
+  flagsState = {
+          state = FLAGS_STATE_RED,
+          toggles = 0
+  }
 }
 
 void setFlagsGreen() {
+  flagsState = {
+          state = FLAGS_STATE_GREEN,
+          toggles = 0
+  }
+}
+
+void updateFlagsGreen() {
+  if (flagsState.state != FLAGS_STATE_GREEN || flagsState.toggles > (5 * 2 - 1)) {
+    return;
+  }
+
+  bool on = flagsState.toggles % 2 == 0;
   flags.clear();
-  flags.fill(flags.Color(0, 255, 0), 0, FLAGS_NUMPIXELS);
+  if (on) {
+    flags.fill(flags.Color(0, 255, 0), 0, FLAGS_NUMPIXELS);
+  }
   flags.show();
+
+  flagsState.toggles++;
+}
+
+void updateFlagsRed() {
+  if (flagsState.state != FLAGS_STATE_RED || flags.flagsState.toggles > 0) {
+    return;
+  }
+
+  flags.clear();
+  flags.fill(flags.Color(255, 0, 0), 0, FLAGS_NUMPIXELS);
+  flags.show();
+
+  flagsState.toggles++;
+}
+
+void updateFlagsChaos() {
+  if (flagsState.state != FLAGS_STATE_CHAOS) {
+    return;
+  }
+
+  int shift = flagsState.toggles % 2;
+  flags.clear();
+
+  for (int flag = 0; flag < FLAGS_NUM; flag++) {
+    for (int pixel = shift; pixel < FLAGS_PIXELS_PER_FLAG; pixel += 2) {
+      int index = flag * FLAGS_PIXELS_PER_FLAG + pixel;
+
+      flags.setPixelColor(index, flags.Color(255, 255, 0));
+    }
+  }
+
+  flags.show();
+
+  flagsState.toggles = (flagsState.toggles + 1) % 2;
 }
 
 // --- inputs ---
@@ -183,7 +281,7 @@ void execReadFlagPins() {
   int value1 = digitalRead(PIN_FLAG1);
   int value2 = digitalRead(PIN_FLAG2);
 
-  switch(value1 + value2 * 2) {
+  switch (value1 + value2 * 2) {
     case 0:
       setFlagsRed();
       break;
@@ -191,7 +289,7 @@ void execReadFlagPins() {
       setFlagsGreen();
       break;
     case 2:
-      setFlagsYellow();
+      setFlagsChaos();
       break;
   }
 }
