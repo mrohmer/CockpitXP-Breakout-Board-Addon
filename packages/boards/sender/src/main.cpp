29ead6dbdccac944401f266f1f190f20cb5f5602
mrohmer <rohmer.matze@gmail.com>
2025-11-18 12:57:06 +0100

    chore(sender): make wifi configurable

--- packages/boards/sender/src/main.cpp
+++ packages/boards/sender/src/main.cpp
@@ -4,16 +4,20 @@
 
 #include <Arduino.h>
 #include "secrets.hpp"
-#include "communication/server/Server.h"
 #include "input/Input.h"
 #include "Led.h"
 #include "communication/Now.h"
 #include "Controller.h"
 #include "Flags.h"
-#include <WiFi.h>
 
 #define CHANNEL 0
 #define HOSTNAME "carrera"
+
+#ifdef USE_WIFI
+  #include <WiFi.h>
+  #include "communication/server/Server.h"
+  HttpServer server;
+#endif
 
 #ifdef USE_I2C_INPUT
 #include "input/I2CInput.h"
@@ -23,7 +27,6 @@ Input* input = new I2CInput((uint8_t)0x55, PIN_SDA, PIN_SCL);
 Input* input = new UsbBoxInput(PIN_FLAG1, PIN_FLAG2, PIN_SESSION_RECORD);
 #endif
 
-HttpServer server;
 Controller controller(input, new Now(CHANNEL));
 Led internalLed(INTERNAL_LED_PIN);
 
@@ -38,6 +41,7 @@ void setup() {
   internalLed.init();
   internalLed.on();
 
+#ifdef USE_WIFI
   WiFi.setHostname(HOSTNAME);
   WiFi.begin(SECRET_WIFI_SSID, SECRET_WIFI_PASSKEY);
   WiFi.setAutoReconnect(true);
@@ -47,6 +51,7 @@ void setup() {
     .onUpdateProgress(std::bind(&Controller::setUpdateProgress, &controller, std::placeholders::_1))
     ->onUpdateDone(std::bind(&Controller::setUpdateDone, &controller))
     ->init();
+#endif
 
   bool success = controller.addFlag(new Flags(FLAGS_PIN))
 #ifdef INTERNAL_RGB_LED_PIN
@@ -63,6 +68,9 @@ void setup() {
 void loop() {
   internalLed.toggle();
   controller.loop();
+
+#ifdef USE_WIFI
   server.loop();
+#endif
   delay(100);
 }
