f8204c9e00377615b73d173056b9e87c6806e926
mrohmer <rohmer.matze@gmail.com>
2025-11-18 11:55:09 +0100

    feat(flags): support alpha colors

--- packages/boards/flag/src/utils/ParseColor.cpp
+++ packages/boards/flag/src/utils/ParseColor.cpp
@@ -21,6 +21,18 @@ optional<Color> parseColor(const std::string& str) {
                 1
             }};
         }
+    } else if (str.length() == 4) {
+        int64_t iv = parseInt(str, 16);
+        if (!(iv >= 0 && iv <= 0xfff)) {
+            return {};
+        } else {
+            return {{
+                static_cast<uint8_t>(((iv & 0xf000) >> 8) | ((iv & 0xf000) >> 12)),
+                static_cast<uint8_t>(((iv & 0xf00) >> 4) | ((iv & 0xf00) >> 8)),
+                static_cast<uint8_t>((iv & 0xf0) | ((iv & 0xf0) >> 4)),
+                static_cast<uint8_t>((iv & 0xf) | ((iv & 0xf) << 4)),
+            }};
+        }
     } else if (str.length() == 6) {
         int64_t iv = parseInt(str, 16);
         if (!(iv >= 0 && iv <= 0xffffff)) {
@@ -31,6 +43,18 @@ optional<Color> parseColor(const std::string& str) {
                 static_cast<uint8_t>((iv & 0xff00) >> 8),
                 static_cast<uint8_t>(iv & 0xff),
                 1
+            }};
+        }
+    }else if (str.length() == 8) {
+        int64_t iv = parseInt(str, 16);
+        if (!(iv >= 0 && iv <= 0xffffff)) {
+            return {};  // Covers NaN.
+        } else {
+            return {{
+                static_cast<uint8_t>((iv & 0xff000000) >> 24),
+                static_cast<uint8_t>((iv & 0xff0000) >> 16),
+                static_cast<uint8_t>((iv & 0xff00) >> 8),
+                static_cast<uint8_t>(iv & 0xff)
             }};
         }
     }
