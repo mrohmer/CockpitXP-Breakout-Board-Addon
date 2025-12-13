dc13b1c633f4155bce3689aa518b62e9c2142a25
mrohmer <rohmer.matze@gmail.com>
2025-12-13 15:04:58 +0100

    feat: implement debounced settings

--- packages/boards/sender/src/utils/Settings.cpp
+++ packages/boards/sender/src/utils/Settings.cpp
@@ -9,10 +9,24 @@ Settings* Settings::instance = nullptr;
 void Settings::init() {
     this->preferences.begin("cmacenter", false);
 }
-
+void Settings::restartTicker() {
+    ticker.detach();
+    ticker.once_ms(30000, std::bind(&Settings::flush, this));
+}
+void Settings::flush() {
+    ticker.detach();
+    for (const auto& pair : this->debouncedInts) {
+        this->preferences.putInt(pair.first.c_str(), pair.second);
+    }
+    this->debouncedInts.clear();
+}
 void Settings::setFlagBrightness(int brightness) {
-    this->preferences.putInt("FLAG_BRIGHTNESS", std::min(std::max(brightness, 0), 255));
+    this->restartTicker();
+    this->debouncedInts[KEY_FLAG_BRIGHTNESS] = std::min(std::max(brightness, 0), 255);
 }
 int Settings::getFlagBrightness() {
-    return this->preferences.getInt("FLAG_BRIGHTNESS", 34); // 22
+    if (this->debouncedInts.find(KEY_FLAG_BRIGHTNESS) != this->debouncedInts.end()) {
+        return this->debouncedInts[KEY_FLAG_BRIGHTNESS];
+    }
+    return this->preferences.getInt(KEY_FLAG_BRIGHTNESS, 34); // 22
 }
--- packages/boards/sender/src/utils/Settings.h
+++ packages/boards/sender/src/utils/Settings.h
@@ -6,11 +6,19 @@
 #define COCKPITXP_BREAKOUT_BOARD_ADDON_SETTINGS_H
 
 #include <Preferences.h>
+#include <map>
+#include "Ticker.h"
+
+#define KEY_FLAG_BRIGHTNESS "FLAG_BRIGHTNESS"
 
 class Settings {
 private:
     static Settings* instance;
+    Ticker ticker;
+    std::map<String, int> debouncedInts;
     Preferences preferences;
+    void restartTicker();
+    void flush();
 public:
     static Settings* getInstance() {
         if (!instance) {
